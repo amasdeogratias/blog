49b0e83d013adb2dd07fb68ec6f061188a205060
fixnetcodes <deogratiasprog@gmail.com>
2024-05-02 14:42:17 +0300

    added random posts and css

--- index.php
+++ index.php
@@ -24,6 +24,74 @@ include './partials/header.php';
             </div>
         </div>
     </section>
+    
+    <section class="posts">
+        <div class="container posts__container">
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog3.jpg" alt="">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.php" class="category__button">Post category</a>
+                    <h2 class="post__title"><a href="post.php">Post title</a> </h2>
+                    <p class="post__body">
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar3.jpg">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Deo Amas</h5>
+                            <small>June 10, 2022 -07:23</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog4.jpg" alt="">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.php" class="category__button">Post category</a>
+                    <h2 class="post__title"><a href="post.php">Post title</a> </h2>
+                    <p class="post__body">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar4.jpg">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Deo Amas</h5>
+                            <small>June 10, 2022 -07:23</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog5.jpg" alt="">
+                </div>
+                <div class="post__info">
+                    <a href="category-posts.php" class="category__button">Post category</a>
+                    <h2 class="post__title"><a href="post.php">Post title</a> </h2>
+                    <p class="post__body">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar5.jpg">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Deo Amas</h5>
+                            <small>June 10, 2022 -07:23</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div> 
+    </section>
 <?php
 include './partials/footer.php';
 ?>
