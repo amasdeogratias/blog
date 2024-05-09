ce29c530a5c6581367a392256590d08831b1d4b4
fixnetcodes <deogratiasprog@gmail.com>
2024-05-09 17:17:29 +0300

    modified sidebar

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -6,28 +6,22 @@ include 'partials/header.php';
         <aside>
             <ul>
                 <li>
-                    <a href=""><i class="uil uil-pen"></i> </a>
-                    <h5>Add Post</h5>
+                    <a href=""><i class="uil uil-pen"></i> <h5>Add Post</h5></a>
                 </li>
                 <li>
-                    <a href=""><i class="uil uil-postcard"></i> </a>
-                    <h5>Manage Posts</h5>
+                    <a href=""><i class="uil uil-postcard"></i> <h5>Manage Posts</h5></a>
                 </li>
                 <li>
-                    <a href=""><i class="uil uil-user-plus"></i> </a>
-                    <h5>Add User</h5>
+                    <a href=""><i class="uil uil-user-plus"></i> <h5>Add User</h5></a>
                 </li>
                 <li>
-                    <a href="manage-users.php"><i class="uil uil-list-ul"></i> </a>
-                    <h5>Manage Users</h5>
+                    <a href="manage-users.php"><i class="uil uil-list-ul"></i> <h5>Manage Users</h5></a>
                 </li>
                 <li>
-                    <a href=""><i class="uil uil-edit"></i> </a>
-                    <h5>Add Category</h5>
+                    <a href=""><i class="uil uil-edit"></i> <h5>Add Category</h5></a>
                 </li>
                 <li>
-                    <a href=""><i class="uil uil-list-ul"></i> </a>
-                    <h5>Manage Categories</h5>
+                    <a href=""><i class="uil uil-list-ul"></i> <h5>Manage Categories</h5></a>
                 </li>
             </ul>
         </aside>
