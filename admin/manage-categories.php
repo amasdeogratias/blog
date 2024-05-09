2dd23a88f317ccda2ecd51da773085cb8e853637
fixnetcodes <deogratiasprog@gmail.com>
2024-05-09 17:14:43 +0300

    added side bar for admin dashboard

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -1,1 +1,68 @@
 <?php
+include 'partials/header.php';
+?>
+<section class="dashboard">
+    <div class="container dashboard__container">
+        <aside>
+            <ul>
+                <li>
+                    <a href=""><i class="uil uil-pen"></i> </a>
+                    <h5>Add Post</h5>
+                </li>
+                <li>
+                    <a href=""><i class="uil uil-postcard"></i> </a>
+                    <h5>Manage Posts</h5>
+                </li>
+                <li>
+                    <a href=""><i class="uil uil-user-plus"></i> </a>
+                    <h5>Add User</h5>
+                </li>
+                <li>
+                    <a href="manage-users.php"><i class="uil uil-list-ul"></i> </a>
+                    <h5>Manage Users</h5>
+                </li>
+                <li>
+                    <a href=""><i class="uil uil-edit"></i> </a>
+                    <h5>Add Category</h5>
+                </li>
+                <li>
+                    <a href=""><i class="uil uil-list-ul"></i> </a>
+                    <h5>Manage Categories</h5>
+                </li>
+            </ul>
+        </aside>
+        <main>
+            <h2>Manage Categories</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Travel</td>
+                        <td><a href="" class="btn sm">Edit</a> </td>
+                        <td><a href="" class="btn sm danger">Delete</a> </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Travel</td>
+                        <td>
+                            <a href="" class="btn sm">Edit</a>
+                            <a href="" class="btn sm danger">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+    </div>
+</section>
+
+<?php
+include 'partials/footer.php';
+?>
+
