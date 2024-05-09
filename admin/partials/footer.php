a39109c11fcb0be4038998e37d88ab81d21072d4
fixnetcodes <deogratiasprog@gmail.com>
2024-05-09 16:18:33 +0300

    added admin footer

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,1 +1,57 @@
 <?php
+?>
+<footer>
+    <div class="footer__socials">
+        <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i> </a>
+        <a href="https://linkedin.com" target="_blank"><i class="uil uil-linkedin"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
+    </div>
+    <div class="container footer__container">
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Art</a> </li>
+                <li><a href="">Wild life</a> </li>
+                <li><a href="">Travel</a> </li>
+                <li><a href="">Music</a> </li>
+                <li><a href="">Technology</a> </li>
+                <li><a href="">Food</a> </li>
+            </ul>
+        </article>
+        <article>
+            <h4>Support</h4>
+            <ul>
+                <li><a href="">Online Support</a> </li>
+                <li><a href="">Call</a> </li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">Safety</a> </li>
+                <li><a href="">Repair</a> </li>
+                <li><a href="">Recent</a> </li>
+                <li><a href="">Popular</a> </li>
+                <li><a href="">Categories</a> </li>
+            </ul>
+        </article>
+        <article>
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="">Home</a> </li>
+                <li><a href="">Blog</a> </li>
+                <li><a href="">About</a> </li>
+                <li><a href="">Services</a> </li>
+                <li><a href="">Contact</a> </li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__copyright">
+        <small>Copyright &copy; 2024 Aditech</small>
+    </div>
+</footer>
+
+<script src="<?php echo ROOT_URL ?>js/main.js"></script>
+</body>
+</html>
+
