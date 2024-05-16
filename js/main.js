b6c664a66233b1f89dad02c53cfa812d399dc65c
fixnetcodes <deogratiasprog@gmail.com>
2024-05-16 14:56:32 +0300

    control admin sidebar on small devices using javascript

--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ const closeNav = () => {
 }
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+
+const closeSidebar = () => {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', closeSidebar);
