3c5ff2cc17a4b0e315b2b1fa0c531b05e3b84b50
fixnetcodes <deogratiasprog@gmail.com>
2024-05-16 14:54:42 +0300

    modified admin dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,33 +1,13 @@
 <?php
-include 'partials/header.php';
+include './layouts/header.php';
 ?>
 <section class="dashboard">
     <div class="container dashboard__container">
-        <aside>
-            <ul>
-                <li>
-                    <a href="dashboard.php"><i class="uil uil-pen"></i> <h5>Dashboard</h5></a>
-                </li>
-                <li>
-                    <a href=""><i class="uil uil-pen"></i> <h5>Add Post</h5></a>
-                </li>
-                <li>
-                    <a href=""><i class="uil uil-postcard"></i> <h5>Manage Posts</h5></a>
-                </li>
-                <li>
-                    <a href=""><i class="uil uil-user-plus"></i> <h5>Add User</h5></a>
-                </li>
-                <li>
-                    <a href="manage-users.php"><i class="uil uil-list-ul"></i> <h5>Manage Users</h5></a>
-                </li>
-                <li>
-                    <a href=""><i class="uil uil-edit"></i> <h5>Add Category</h5></a>
-                </li>
-                <li>
-                    <a href=""><i class="uil uil-list-ul"></i> <h5>Manage Categories</h5></a>
-                </li>
-            </ul>
-        </aside>
+        <button class="sidebar__toggle" id="show__sidebar-btn"><i class="uil uil-angle-right-b"></i></button>
+        <button class="sidebar__toggle" id="hide__sidebar-btn"><i class="uil uil-angle-left-b"></i></button>
+        <?php
+        include './layouts/sidebar.php';
+        ?>
         <main>
             <h2>Manage Categories</h2>
             <table>
@@ -42,12 +22,14 @@ include 'partials/header.php';
                 <tr>
                     <td>1</td>
                     <td>Travel</td>
-                    <td><a href="" class="btn sm">Edit</a> </td>
-                    <td><a href="" class="btn sm danger">Delete</a> </td>
+                    <td>
+                        <a href="" class="btn sm">Edit</a>
+                        <a href="" class="btn sm danger">Delete</a>
+                    </td>
                 </tr>
                 <tr>
                     <td>2</td>
-                    <td>Travel</td>
+                    <td>Wild Life</td>
                     <td>
                         <a href="" class="btn sm">Edit</a>
                         <a href="" class="btn sm danger">Delete</a>
@@ -60,6 +42,6 @@ include 'partials/header.php';
 </section>
 
 <?php
-include 'partials/footer.php';
+include './layouts/footer.php';
 ?>
 
