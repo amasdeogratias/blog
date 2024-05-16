6b7560d85aad4dd48f2718355c55403b730ca175
fixnetcodes <deogratiasprog@gmail.com>
2024-05-16 14:57:08 +0300

    added sidebar layout for admin panel

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 require './config/database.php';
+if(!isset($_SESSION['user_id'])){
+    header("location:" . ROOT_URL . 'index.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -1,1 +1,25 @@
-<?php
+<aside>
+    <ul>
+        <li>
+            <a href="dashboard.php" class="active"><i class="uil uil-pen"></i> <h5>Dashboard</h5></a>
+        </li>
+        <li>
+            <a href=""><i class="uil uil-pen"></i> <h5>Add Post</h5></a>
+        </li>
+        <li>
+            <a href=""><i class="uil uil-postcard"></i> <h5>Manage Posts</h5></a>
+        </li>
+        <li>
+            <a href=""><i class="uil uil-user-plus"></i> <h5>Add User</h5></a>
+        </li>
+        <li>
+            <a href="manage-users.php"><i class="uil uil-list-ul"></i> <h5>Manage Users</h5></a>
+        </li>
+        <li>
+            <a href=""><i class="uil uil-edit"></i> <h5>Add Category</h5></a>
+        </li>
+        <li>
+            <a href="manage-categories.php"><i class="uil uil-list-ul"></i> <h5>Manage Categories</h5></a>
+        </li>
+    </ul>
+</aside>
