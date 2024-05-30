ffff5c99d9e99e2c4496404e649f279a6d3dca87
fixnetcodes <deogratiasprog@gmail.com>
2024-05-30 15:21:16 +0300

    corrected cross browser issue caused by meta

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['user_id'])){
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>BLOG</title>
 
     <!-- Custom stylesheet -->
