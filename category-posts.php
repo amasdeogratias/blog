41346d75b0570f371f67d60a4e5e719e52ec8366
DeogratiasAmas <deogratiasprog@gmail.com>
2024-10-25 09:56:41 +0300

    Displayed all posts by category id

--- category-posts.php
+++ category-posts.php
@@ -1,33 +1,60 @@
 <?php
 include './partials/header.php';
+if(isset($_GET['category_id'])){
+    $category_id = filter_var($_GET['category_id'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $query = "SELECT * FROM posts WHERE category_id=$category_id ORDER BY created_at DESC";
+    $result = mysqli_query($connect, $query);
+
+    $category_title_query = "SELECT title FROM categories WHERE id=$category_id";
+    $category_title_result = mysqli_query($connect, $category_title_query);
+    if(mysqli_num_rows($category_title_result) == 1){
+        $category_data = mysqli_fetch_assoc($category_title_result);
+    }
+
+}else{
+    header("location:" .ROOT_URL);
+    die();
+}
 ?>
 
     <header class="category__title">
-        <h1>Category Title</h1>
+        <h1><?php echo $category_data['title'] ?></h1>
     </header>
     <section class="posts">
         <div class="container posts__container">
+        <?php while($rows = mysqli_fetch_assoc($result)){
+                $category_id = $rows['category_id'];
+                $category_query = "SELECT id, title FROM categories WHERE id=$category_id";
+                $category_result = mysqli_query($connect, $category_query);
+                $category = mysqli_fetch_assoc($category_result);
+
+                $author_id = $rows['author_id'];
+                $author_query = "SELECT firstname, lastname FROM users WHERE id=$author_id";
+                $author_result = mysqli_query($connect, $author_query);
+                $author = mysqli_fetch_assoc($author_result);
+                 ?>
             <article class="post">
                 <div class="post__thumbnail">
-                    <img src="">
+                    <img src="./images/<?php echo $rows['thumbnail'] ?>" alt="">
                 </div>
                 <div class="post__info">
-                    <a href="category-posts.php" class="category__button">Post category</a>
-                    <h2 class="post__title"><a href="post.php">Post title</a> </h2>
+                    <a href="category-posts.php?category_id=<?= $category['id']?>" class="category__button"><?= $category['title']?></a>
+                    <h2 class="post__title"><a href="post.php"><?= $rows['title'] ?></a> </h2>
                     <p class="post__body">
-                        lorem ipsum dolor sit amet consectectur
+                    <?= substr($rows['body'], 0,300) ?>...
                     </p>
                     <div class="post__author">
                         <div class="post__author-avatar">
-                            <img src="./images/avatar2.jpg">
+                            <img src="./images/avatar3.jpg">
                         </div>
                         <div class="post__author-info">
-                            <h5>By: Deo Amas</h5>
-                            <small>June 10, 2022 -07:23</small>
+                            <h5>By: <?= $author['firstname'] . ' ' .$author['lastname'] ?></h5>
+                            <small style="color:white"><?php echo date('r', strtotime($rows['created_at'])) ?></small>
                         </div>
                     </div>
                 </div>
             </article>
+            <?php } ?>
         </div>
     </section>
 
--- index.php
+++ index.php
@@ -20,11 +20,11 @@ $categories_result = mysqli_query($connect, $categories_query);
             <div class="post__info">
                 <?php 
                     $category_id = $featured['category_id'];
-                    $category_query = "SELECT title FROM categories WHERE id=$category_id";
+                    $category_query = "SELECT id, title FROM categories WHERE id=$category_id";
                     $category_result = mysqli_query($connect, $category_query);
                     $category = mysqli_fetch_assoc($category_result);
                 ?>
-                <a href="category-posts.php" class="category__button"><?php echo $category['title'] ?></a>
+                <a href="category-posts.php?category_id=<?php echo $category['id'] ?>" class="category__button"><?php echo $category['title'] ?></a>
                 <h2 class="post__title"><a href="post.php"><?php echo $featured['title'] ?></a> </h2>
                 <p class="post__body">
                 <?php echo $featured['body'] ?>
@@ -59,7 +59,7 @@ $categories_result = mysqli_query($connect, $categories_query);
         <div class="container posts__container">
             <?php while($rows = mysqli_fetch_assoc($result)){
                 $category_id = $rows['category_id'];
-                $category_query = "SELECT title FROM categories WHERE id=$category_id";
+                $category_query = "SELECT id, title FROM categories WHERE id=$category_id";
                 $category_result = mysqli_query($connect, $category_query);
                 $category = mysqli_fetch_assoc($category_result);
 
@@ -73,7 +73,7 @@ $categories_result = mysqli_query($connect, $categories_query);
                     <img src="./images/<?php echo $rows['thumbnail'] ?>" alt="">
                 </div>
                 <div class="post__info">
-                    <a href="category-posts.php" class="category__button"><?= $category['title']?></a>
+                    <a href="category-posts.php?category_id=<?= $category['id']?>" class="category__button"><?= $category['title']?></a>
                     <h2 class="post__title"><a href="post.php"><?= $rows['title'] ?></a> </h2>
                     <p class="post__body">
                     <?= substr($rows['body'], 0,300) ?>...
@@ -96,7 +96,7 @@ $categories_result = mysqli_query($connect, $categories_query);
     <section class="category__buttons">
         <div class="container category__buttons-container">
             <?php while($rows = mysqli_fetch_assoc($categories_result)){ ?>
-            <a href="" class="category__button"><?= $rows['title'] ?></a>
+            <a href="category-posts.php?category_id=<?= $rows['id'] ?>" class="category__button"><?= $rows['title'] ?></a>
             <?php } ?>
             
         </div>
