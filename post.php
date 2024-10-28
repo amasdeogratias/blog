2f2681e93cb35f1c646efdf4a51b106e0e667e47
DeogratiasAmas <deogratiasprog@gmail.com>
2024-10-28 15:56:12 +0300

    structured single post and its css

---
+++ post.php
@@ -0,0 +1,26 @@
+<?php
+include './partials/header.php';
+?>
+    <section class="singlepost">
+        <div class="container singlepost__container">
+            <h2 class="post__title"><a href="post.php">Post title</a> </h2>
+            <div class="post__author">
+                <div class="post__author-avatar">
+                    <img src="./images/1729251288avatar.png">
+                </div>
+                <div class="post__author-info">
+                    <h5>By: Deo Amas</h5>
+                    <small style="color:white">June 10, 2022 -07:23</small>
+                </div>
+            </div>
+            <div class="singlepost__thumbnail">
+                <img src="./images/blog11.jpg">
+            </div>
+            <p class="post__body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor tempora vitae id fugit repellat odit blanditiis! Magni mollitia ut soluta dolore, ipsa expedita, illo debitis quaerat enim maxime libero, dolor aperiam veritatis odio. Reiciendis laudantium illum quas unde doloremque, veritatis, tenetur blanditiis minus voluptatibus, labore sunt atque quam itaque magni dicta. Eius architecto nulla blanditiis et? Voluptatibus doloribus nemo perspiciatis consequuntur, assumenda architecto possimus eligendi quos animi quis libero sed totam odio voluptas, ipsum rerum magnam inventore, vero laborum reprehenderit ad numquam commodi asperiores. Animi unde nam atque eaque, recusandae excepturi quis nemo inventore tempora ducimus ea autem, porro mollitia enim voluptas neque? Quae numquam atque sunt maiores, ducimus deleniti porro fugiat nostrum illum facilis quibusdam aliquid hic laboriosam quia repudiandae veritatis est sequi cupiditate nobis cumque quos? Nostrum maxime quaerat officiis quisquam iste porro a laborum odio dolores voluptatum iure ex facere impedit tempora, libero harum delectus inventore repellat nisi? Eveniet tenetur, rerum unde blanditiis eius, praesentium dolores doloribus cumque quidem quam commodi facilis dolorum vitae quasi eaque adipisci placeat similique hic neque fugit voluptas ipsum aut reprehenderit? Officiis tenetur quod quis facere in beatae dolorem at nisi eligendi cupiditate quae dignissimos necessitatibus impedit eius, deleniti vitae accusamus ut dolorum. Laborum perspiciatis ad ipsum quasi dignissimos aut quia? Sed obcaecati quos, suscipit quidem facilis hic tempora libero accusamus saepe aperiam similique placeat, et quia aliquam perspiciatis nemo? Ratione facere quis maiores nostrum reiciendis sit et, esse odio perspiciatis error corporis dicta! Illum, dignissimos explicabo! Aspernatur, sed. Vel ducimus iure esse obcaecati cupiditate, deleniti minima cum debitis, nihil eius sunt quo quos perferendis quisquam error, nulla laborum repellat mollitia quis ab provident itaque rerum dolorem. Laboriosam natus ipsam doloribus labore officia provident qui vero doloremque molestiae, nulla deleniti? Aut, ratione eos. Non nobis fugiat, illo dolores reprehenderit esse iusto suscipit minus, sit minima ipsa quas repudiandae veniam doloremque magni inventore molestiae, assumenda soluta at! Voluptas incidunt voluptatibus iste at quam laudantium et non perspiciatis, tempore cupiditate eius dolore expedita magni totam consequatur debitis minus ea sit facere. Magnam dolorem quod sunt repudiandae eveniet, aut, molestiae exercitationem, id tenetur tempore ducimus rem officiis. Perspiciatis, corporis! Aut fuga porro fugit nulla minima qui tempore debitis neque sint a vel maiores temporibus totam eos deleniti ea omnis perspiciatis incidunt laboriosam at sit nobis, ducimus exercitationem? Illum quidem, facilis numquam unde labore id porro voluptatem corporis iusto voluptatum, magnam ratione neque accusamus, quam similique autem? Repudiandae quibusdam facilis animi aspernatur iusto ea, suscipit expedita similique nulla, nam, cum quam quo molestias cupiditate. Est nesciunt odit in autem porro assumenda ratione dolore reiciendis vitae aliquid laudantium quia aperiam architecto possimus voluptatibus commodi, perferendis, dolores laboriosam nihil consectetur. Animi praesentium molestias esse perspiciatis minima! Voluptate minima tempore reprehenderit maiores nemo tenetur sapiente cupiditate. Odit nulla deserunt nihil, reprehenderit aperiam similique enim ut excepturi impedit et provident sint maxime ipsam possimus error labore quisquam illum. Eligendi neque nemo debitis, repellendus nesciunt distinctio consequuntur, ipsa voluptatum provident ad optio nisi nulla nobis perferendis sit officia possimus tempora iusto.
+            </p>
+        </div>
+    </section>
+<?php
+include './partials/footer.php';
+?>
